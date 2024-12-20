e6f29ec3aec202ce07c5485a22e95de0ca2dbf59
ZeusKotGS <nhanp.truong@gmail.com>
2024-12-20 14:20:24 -0800

    added middlewares and listeners

---
+++ backend/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express'
+
+const app = express(); //create app
+
+//middlewares
+app.use(express.json());
+
+//connections and listeners
+app.listen(5001, () => console.log("Server Open")); //listen to port 5001
