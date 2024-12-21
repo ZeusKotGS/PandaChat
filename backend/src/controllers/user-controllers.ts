9cd5796dca9f414e205bd24174794af1a9dab779
ZeusKotGS <nhanp.truong@gmail.com>
2024-12-20 20:37:46 -0800

    auth and cookies

--- backend/src/controllers/user-controllers.ts
+++ backend/src/controllers/user-controllers.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from "express";
 import User from "../models/User.js";
 import { hash, compare } from 'bcrypt';
+import { createToken } from "../utils/token-manager.js";
+import { COOKIE_NAME } from "../utils/constants.js";
 
 export const getAllUsers = async (req: Request, res: Response, next: NextFunction) => {
     try {
@@ -23,6 +25,26 @@ export const userSignup = async (req: Request, res: Response, next: NextFunction
         const hashedPassword = await hash(password, 10);
         const user = new User({name, email, password: hashedPassword});
         await user.save();
+
+        // create token and store cookie
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/"
+        });
+
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7)
+        res.cookie(COOKIE_NAME, token, {
+            path: "/", 
+            domain: "localhost", 
+            expires, 
+            httpOnly: true, 
+            signed: true
+        });
+
         return res.status(201).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
         console.log(error);
@@ -42,6 +64,26 @@ export const userLogin = async (req: Request, res: Response, next: NextFunction)
         if(!isPasswordCorrect) {
             return res.status(403).send("Incorrect Password");
         }
+
+        // create and store cookies
+        res.clearCookie(COOKIE_NAME, {
+            httpOnly: true,
+            domain: "localhost",
+            signed: true,
+            path: "/"
+        });
+
+        const token = createToken(user._id.toString(), user.email, "7d");
+        const expires = new Date();
+        expires.setDate(expires.getDate() + 7)
+        res.cookie(COOKIE_NAME, token, {
+            path: "/", 
+            domain: "localhost", 
+            expires, 
+            httpOnly: true, 
+            signed: true
+        });
+
         return res.status(201).json({ message: "OK", id: user._id.toString() });
     } catch (error) {
         console.log(error);
